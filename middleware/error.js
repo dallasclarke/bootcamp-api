944132558568386b8fd3e94f75c4de9092bc0742
Dallas <dallas.clarke622@gmail.com>
2021-07-16 15:31:23 -0700

    Created another error handler for validation. Also created asyncHandler in middleware to help clean things up in controller

--- middleware/error.js
+++ middleware/error.js
@@ -7,9 +7,22 @@ const errorHandler = (err, req, res, next) => {
 
   error.message = err.message;
 
+  // Bad ObjectId
   if (err.name === "CastError") {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Duplicate key
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, 400)
+  }
+
+  // Validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400)
   }
 
   res
