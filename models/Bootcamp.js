79aea8c296652ffde7828adacc0ebe8698786675
Dallas <dallas.clarke622@gmail.com>
2021-07-16 16:03:29 -0700

    Using mongoose middleware to create a slug when creating a new bootcamp

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -98,6 +99,12 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+BootcampSchema.pre("save", function (next) {
+  // console.log("Slugify ran", this.name);
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
